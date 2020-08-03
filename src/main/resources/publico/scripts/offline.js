79e39db990a5354c209d07208d33ca9971be2811
Armando R <20160415@ce.pucmm.edu.do>
2020-08-03 11:52:29 -0400

    funciones offline en FrontEnd y guardado de pagina en LocalStorage

--- src/main/resources/publico/scripts/offline.js
+++ src/main/resources/publico/scripts/offline.js
@@ -7,10 +7,47 @@ window.addEventListener('offline', updateOnlineStatus);
 function updateOnlineStatus(event) {
     var condition = navigator.onLine ? "online" : "offline";
     document.body.className = condition;
+    indicador(condition);
 }
 
-function waiting(event) {
-    var mirador = document.body.className
+//FUNCIÓN PARA EL FRONTEND
+function indicador(condition) {
 
+    if (condition === "online") {
+        ///alerta para el usuario
+        var signal = document.getElementById("demo");
+        signal.innerHTML = "En linea <i id=\"logo\" class=\"fa fa-check-circle fa-fw\"></i>";
+        signal.setAttribute("class", "btn-success");
+        document.getElementById("logo").setAttribute("class", "fa fa-check-circle fa-fw");
+
+        ///botones para interactual con el servidor
+        document.getElementById("enviarForm").removeAttribute("disabled")
+        document.getElementById("listForm").setAttribute("href","Informe.html");
+    }
+    if (condition === "offline"){
+
+        ///alerta para el usuario
+        var signal = document.getElementById("demo");
+        signal.innerHTML = "Sin Conexión <i id=\"logo\" class=\"fa fa-times-circle fa-fw\"></i>";
+        signal.setAttribute("class", "btn-danger");
+
+        ///botones para interactual con el servidor
+        document.getElementById("enviarForm").setAttribute("disabled", "true");
+        document.getElementById("listForm").setAttribute("href", "#");
+    }
+}
+
+//FUNCION PARA LA UTILIZACIÓN DE WEBSTORAGE
+function Local() {
+    var almacenamiento = localStorage.getItem("BackUp_fecha");
+    if(almacenamiento == null){
+        var pag = document.body;
+        var tmp = new Date().toUTCString();
+        localStorage.setItem("BackUp_fecha", tmp);
+        localStorage.setItem("pagina", pag);
+
+        //Almacenando información en el ambiente de sesión.
+        sessionStorage.setItem("BackUp_fecha", new Date().toUTCString())
+    }
 
 }
